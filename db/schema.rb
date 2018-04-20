66c5abe3d2a4b737e1dbaa7d707cef72476e730d
Joan Currie <jecurrie8@gmail.com>
2018-04-20 11:38:50 -0400

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -20,19 +20,8 @@ ActiveRecord::Schema.define(version: 2018_04_18_185323) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
 
-  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-  end
+   end
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
