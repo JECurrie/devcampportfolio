2fd09dbe5b2888d11c606a159081cef2bcd68fdb
Joan Currie <jecurrie8@gmail.com>
2018-04-21 20:01:14 -0400

    Integrated index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
+  get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
 
-  root to: 'pages#home'
+  #root to: 'pages#home'
 end
